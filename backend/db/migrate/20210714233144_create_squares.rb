a16544e75878e0cdd683ea0ac283e4350dc04224
Adriana <am20dipi@alum.siena.edu>
2021-07-14 19:33:44 -0400

    Creating and migrating the database, creating the schema

--- backend/db/migrate/20210714233144_create_squares.rb
+++ backend/db/migrate/20210714233144_create_squares.rb
@@ -4,7 +4,6 @@ class CreateSquares < ActiveRecord::Migration[6.1]
       t.string :color
       t.integer :height
       t.integer :width
-      t.belongs_to :user_id, null: false, foreign_key: true
 
       t.timestamps
     end
