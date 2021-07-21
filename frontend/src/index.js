8c503eb6ee8b48f331110ae4b0cd3563ae7a537c
Adriana <am20dipi@alum.siena.edu>
2021-07-20 20:11:56 -0400

    Adding signup and login pure functions to index.js, adding their routes to render, creating forms for both signup and login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,50 @@ const Home = () => {
       <h1>Home!</h1>
     </div>
   );
-};
+}
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="email" placeholder="Email" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div><br></br>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <div>
+          <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+          <input type="text" name="email" placeholder="Email" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div><br></br>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <Router>
     <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
   </Router>,
   document.getElementById('root')
 );
