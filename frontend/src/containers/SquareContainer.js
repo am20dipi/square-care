bded09c2706f8a782df494ef6279fae570ec3895
Adriana <am20dipi@alum.siena.edu>
2021-07-23 18:22:00 -0400

    Reconfiguring App.js to accomodate for Navbar, rendering App in index.js using ReactDOM.render, creating containers folder with SquareContainers file

---
+++ frontend/src/containers/SquareContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SquareContainer() {
+      
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import reportWebVitals from './reportWebVitals';
-import Home from './Home'
-import Signup from './Signup'
-import Login from './Login'
-import Navbar from './Navbar'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App'
 
 
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
-ReactDOM.render(
-  <Router>
-    <div>
-      <Navbar/>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={Signup} />
-    </div>
-  </Router>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
